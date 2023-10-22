Fixed energy sampling and add spectral discernible calculation

light_by_light/laser_field.py
@@ -84,8 +84,7 @@ class LaserBG:
         
         return self.nrg_from_eb
 
-    def photon_density(self, preserve_integral=False, fix_resolution_radius=False,
-                       angular_resolution=None, **kwargs):
+    def photon_density(self, preserve_integral=False, **sphmap_params):
         """
         Calculates the differential photon number in momentum space.
         """
@@ -103,16 +102,14 @@ class LaserBG:
             ax.name = 'k' + name
         self.dphoton = dphoton
         
-        if fix_resolution_radius:
-            resolution_radius = np.min([kx.max(), ky.max(), kz.max()])
-        else:
-            resolution_radius = None
+        # if fix_resolution_radius:
+        #     resolution_radius = np.min([kx.max(), ky.max(), kz.max()])
+        # else:
+        #     resolution_radius = None
         
         # map photon density to spherical grid
         dphoton_spherical = field_to_spherical(dphoton, preserve_integral=preserve_integral,
-                                               match_resolution_radius=resolution_radius,
-                                               angular_resolution=angular_resolution,
-                                               **kwargs)
+                                               **sphmap_params)
         k, theta, phi = dphoton_spherical.meshgrid()
         self.dphoton_spherical = dphoton_spherical
         self.k, self.theta, self.phi = dphoton_spherical.grid

light_by_light/optuna_optimization.py
@@ -32,8 +32,10 @@ def get_dependent_params(trial, total_value, params, param_key):
         param_name = f'{laser_key}/{param_key}'
         value = trial.suggest_float(param_name, 0, total_value)
         params[laser_key][param_key] = float(value)
+        params[laser_key]['E0'] = float(W_to_E0(params[laser_key]))
         total_value -= value
     params[lasers[-1]][param_key] = float(total_value)
+    params[lasers[-1]]['E0'] = float(W_to_E0(params[lasers[-1]]))
     trial.set_user_attr(f'{lasers[-1]}/{param_key}', float(total_value))
     return params
 
@@ -70,7 +72,8 @@ def get_trial_params(trial, optuna_params, default_params):
 
 def objective_lbl(trial, default_params, optuna_params, save_path, geometry='xz',
                   obj_param='N_total', low_memory_mode=False, n_threads=12,
-                  pol_idx=0, eps=1e-10):
+                  pol_idx=0, eps=1e-10, discernible_spectral=False,
+                  sphmap_params={'order': 1}):
     '''
     Objective function for optuna optimization.
     
@@ -98,13 +101,16 @@ def objective_lbl(trial, default_params, optuna_params, save_path, geometry='xz'
     # Simulation 
     run_simulation_postprocess(laser_params, save_folder, simbox_params,
                                geometry, low_memory_mode, n_threads,
-                               pol_idx, eps)
+                               pol_idx, eps,
+                               discernible_spectral=discernible_spectral,
+                               sphmap_params=sphmap_params)
     
     # Extracting objective function
     result = np.load(f'{os.path.dirname(save_folder)}/postprocess_data.npz')
     for key in ["N_total", "Nperp_total", "N_disc", "Nperp_disc",
                 "N_disc_num", "Nperp_disc_num"]:
-        trial.set_user_attr(key, float(result[key]))
+        if key in result.keys():
+            trial.set_user_attr(key, float(result[key]))
     return float(result[obj_param])
     
 
@@ -122,6 +128,8 @@ def run_optuna_optimization(default_yaml, optuna_yaml, save_path, eps=1e-10,
     obj_param = default_params['obj_param']
     n_trials = default_params['n_trials']
     consider_endpoints = default_params.get('consider_endpoints', False)
+    sphmap_params = default_params.get('sphmap_params', {'order': 1})
+    discernible_spectral = default_params.get('discernible_spectral', False)
     
     # String formatting for database
     study_name = save_path.split('/')[-2]
@@ -150,7 +158,9 @@ def run_optuna_optimization(default_yaml, optuna_yaml, save_path, eps=1e-10,
                   obj_param=obj_param,
                   low_memory_mode=low_memory_mode,
                   n_threads=n_threads,
-                  pol_idx=pol_idx)
+                  pol_idx=pol_idx,
+                  discernible_spectral=discernible_spectral,
+                  sphmap_params=sphmap_params)
     
     # Let the optimization begin!
     study.optimize(obj, n_trials=n_trials)

light_by_light/postprocess.py
@@ -257,7 +257,7 @@ class SignalAnalyzer:
         
 class SignalAnalyzer_k:
     def __init__(self, file, laser_pol, laser_params, geometry='xz',
-                 bg_params={'order': 1}):
+                 sphmap_params={'order': 1}):
         '''
         Class to calculate signal photons (total and perp) from simulation results.
         Discernible signal is calculated with self.get_discernible_signal().
@@ -267,14 +267,14 @@ class SignalAnalyzer_k:
                     laser polarization vector
         laser_params: [list of dict] - parameters of laser in the collision
         geometry: ['xy' or 'xz'] - collision geometry
-        bg_params: [dict] - params to pass to LaserBG class for numerical background
-                            calculation
+        sphmap_params: [dict] - spherical map params, params to pass to field_to_spherical() 
+                                for map between spherical and cartesian grids
         '''
         self.result = ResultFile(file)
         self.laser_pol = laser_pol
         self.laser_params = laser_params
         self.geometry = geometry
-        self.bg_params = bg_params
+        self.sphmap_params = sphmap_params
         
         self.N_xyz = self.result.get_total_number_spectrum()
         self.Nperp_xyz = self.result.get_number_spectrum_polarized_spherical(laser_pol)
@@ -290,8 +290,8 @@ class SignalAnalyzer_k:
         self.energy_num = self.laser_diagnostics.energy()
         
     def get_spherical_density(self):
-        N = field_to_spherical(self.N_xyz, preserve_integral=False, order=1)
-        Nperp = field_to_spherical(self.Nperp_xyz, preserve_integral=False, order=1)
+        N = field_to_spherical(self.N_xyz, preserve_integral=False, **self.sphmap_params)
+        Nperp = field_to_spherical(self.Nperp_xyz, preserve_integral=False, **self.sphmap_params)
         self.N = N
         self.Nperp = Nperp
         
@@ -327,17 +327,17 @@ class SignalAnalyzer_k:
         self.Nperp_total = Nperp_total_sph
         
     def get_background_num(self):
-        self.laser_diagnostics.photon_density(**self.bg_params)
+        self.laser_diagnostics.photon_density(**self.sphmap_params)
         self.background_num = self.laser_diagnostics.dphoton_angular.matrix
         self.background_sph_num = self.laser_diagnostics.dphoton_spherical.matrix
-        return self.background_num
+        return self.background_sph_num
     
     def get_discernible_area(self, Nbg=None):
         if Nbg is None:
             Nbg = self.get_background_num()
         
-        discernible_area = self.N > Nbg
-        discernible_area_perp = self.Nperp > self.pol_purity*Nbg
+        discernible_area = self.N.matrix > Nbg
+        discernible_area_perp = self.Nperp.matrix > self.pol_purity*Nbg
         # for idx_theta in range(len(self.theta)):
         #     for idx_phi in range(len(self.phi)):
         #         idx = self.N_angular[:,idx_theta,idx_phi] > Nbg[:,idx_theta,idx_phi]
@@ -351,9 +351,9 @@ class SignalAnalyzer_k:
         for i in range(len(self.k)):
             for j in range(len(self.theta)):
                 idx = discernible_area[i,j]
-                N_disc += np.sum(self.N[i,j,idx]) * self.k[i]**2 * np.sin(self.theta[j])
+                N_disc += np.sum(self.N.matrix[i,j,idx]) * self.k[i]**2 * np.sin(self.theta[j])
                 idx = discernible_area_perp[i,j]
-                Nperp_disc += np.sum(self.Nperp[i,j,idx]) * self.k[i]**2 * np.sin(self.theta[j])
+                Nperp_disc += np.sum(self.Nperp.matrix[i,j,idx]) * self.k[i]**2 * np.sin(self.theta[j])
         N_disc = N_disc * self.dk * self.dphi * self.dtheta
         Nperp_disc = Nperp_disc * self.dk * self.dphi * self.dtheta
         return N_disc, Nperp_disc

light_by_light/vacem_simulation.py
@@ -11,7 +11,7 @@ from vacem.support.eval_functions import polarization_vector
 
 from light_by_light.vacem_ini import W_to_E0, create_ini_file
 from light_by_light.utils import read_yaml, get_grid_from_list
-from light_by_light.postprocess import SignalAnalyzer
+from light_by_light.postprocess import SignalAnalyzer, SignalAnalyzer_k
 
 __all__ = ['run_simulation', 'run_simulation_postprocess', 'run_gridscan']
 
@@ -35,7 +35,8 @@ def run_simulation(laser_params, save_path, simbox_params,
 
 def run_simulation_postprocess(laser_params, save_path, simbox_params,
                                geometry='xz', low_memory_mode=False, n_threads=12,
-                               pol_idx=0, eps=1e-10):
+                               pol_idx=0, eps=1e-10, discernible_spectral=False,
+                               sphmap_params={'order': 1}):
     # Make sure the directory exists
     Path(os.path.dirname(save_path)).mkdir(parents=True, exist_ok=True)
     
@@ -55,7 +56,11 @@ def run_simulation_postprocess(laser_params, save_path, simbox_params,
                                     phi/180*np.pi,
                                     beta/180*np.pi)
     vacem_file = f'{os.path.dirname(save_path)}/_vacem.npz'
-    signal_analyzer = SignalAnalyzer(vacem_file, laser_pol, laser_params, geometry)
+    if not discernible_spectral:
+        signal_analyzer = SignalAnalyzer(vacem_file, laser_pol, laser_params, geometry)
+    else:
+        signal_analyzer = SignalAnalyzer_k(vacem_file, laser_pol, laser_params, geometry,
+                                           sphmap_params)
     signal_analyzer.get_discernible_signal()
     signal_analyzer.save_data(save_path)
     return 1